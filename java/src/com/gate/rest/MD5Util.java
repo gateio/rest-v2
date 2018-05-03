ff2eee93a3702796224a76261f05959c593c7e1f
gate.io <32661171+gateio@users.noreply.github.com>
2018-05-03 18:15:21 +0800

    Update MD5Util.java
    removed some comments

--- java/src/com/gate/rest/MD5Util.java
+++ java/src/com/gate/rest/MD5Util.java
@@ -8,40 +8,13 @@ import java.util.List;
 import java.util.Map;
 
 public class MD5Util {
-
-	/**
-	 * 鐢熸垚绛惧悕缁撴灉(鏂扮増鏈娇鐢�)
-	 * 
-	 * @param sArray
-	 *            瑕佺鍚嶇殑鏁扮粍
-	 * @return 绛惧悕缁撴灉瀛楃涓�
-	 */
+	
 	public static String buildMysignV1(Map<String, String> sArray,
 			String secretKey) {
 		String mysign = "";
 		try {
-			String prestr = createLinkString(sArray); // 鎶婃暟缁勬墍鏈夊厓绱狅紝鎸夌収鈥滃弬鏁�=鍙傛暟鍊尖�濈殑妯″紡鐢ㄢ��&鈥濆瓧绗︽嫾鎺ユ垚瀛楃涓�
-			prestr = prestr + "&secret_key=" + secretKey; // 鎶婃嫾鎺ュ悗鐨勫瓧绗︿覆鍐嶄笌瀹夊叏鏍￠獙鐮佽繛鎺ヨ捣鏉�
-			mysign = getMD5String(prestr);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return mysign;
-	}
-	
-	/**
-	 * 鐢熸垚绛惧悕缁撴灉锛堣�佺増鏈娇鐢級
-	 * 
-	 * @param sArray
-	 *            瑕佺鍚嶇殑鏁扮粍
-	 * @return 绛惧悕缁撴灉瀛楃涓�
-	 */
-	public static String buildMysign(Map<String, String> sArray,
-			String secretKey) {
-		String mysign = "";
-		try {
-			String prestr = createLinkString(sArray); // 鎶婃暟缁勬墍鏈夊厓绱狅紝鎸夌収鈥滃弬鏁�=鍙傛暟鍊尖�濈殑妯″紡鐢ㄢ��&鈥濆瓧绗︽嫾鎺ユ垚瀛楃涓�
-			prestr = prestr + secretKey; // 鎶婃嫾鎺ュ悗鐨勫瓧绗︿覆鍐嶄笌瀹夊叏鏍￠獙鐮佺洿鎺ヨ繛鎺ヨ捣鏉�
+			String prestr = createLinkString(sArray);
+			prestr = prestr + "&secret_key=" + secretKey;
 			mysign = getMD5String(prestr);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -49,13 +22,19 @@ public class MD5Util {
 		return mysign;
 	}
 
-	/**
-	 * 鎶婃暟缁勬墍鏈夊厓绱犳帓搴忥紝骞舵寜鐓р�滃弬鏁�=鍙傛暟鍊尖�濈殑妯″紡鐢ㄢ��&鈥濆瓧绗︽嫾鎺ユ垚瀛楃涓�
-	 * 
-	 * @param params
-	 *            闇�瑕佹帓搴忓苟鍙備笌瀛楃鎷兼帴鐨勫弬鏁扮粍
-	 * @return 鎷兼帴鍚庡瓧绗︿覆
-	 */
+	public static String buildMysign(Map<String, String> sArray,
+			String secretKey) {
+		String mysign = "";
+		try {
+			String prestr = createLinkString(sArray); 
+			prestr = prestr + secretKey; 
+			mysign = getMD5String(prestr);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return mysign;
+	}
+
 	public static String createLinkString(Map<String, String> params) {
 
 		List<String> keys = new ArrayList<String>(params.keySet());
@@ -64,7 +43,7 @@ public class MD5Util {
 		for (int i = 0; i < keys.size(); i++) {
 			String key = keys.get(i);
 			String value = params.get(key);
-			if (i == keys.size() - 1) {// 鎷兼帴鏃讹紝涓嶅寘鎷渶鍚庝竴涓�&瀛楃
+			if (i == keys.size() - 1) {
 				prestr = prestr + key + "=" + value;
 			} else {
 				prestr = prestr + key + "=" + value + "&";
@@ -73,9 +52,6 @@ public class MD5Util {
 		return prestr;
 	}
 
-	/**
-	 * 鐢熸垚32浣嶅ぇ鍐橫D5鍊�
-	 */
 	private static final char HEX_DIGITS[] = { '0', '1', '2', '3', '4', '5',
 			'6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
