636330f59dcb9e9c1c562d6e13152fc6712f4b88
kefengxu <52153742+kefengxu@users.noreply.github.com>
2019-08-27 15:45:16 +0800

    Update gate.js
    gateio.io 修改为 gateio.co

--- nodejs/lib/gate.js
+++ nodejs/lib/gate.js
@@ -6,8 +6,8 @@ var querystring = require('querystring');
 const KEY  = 'your key';
 const SECRET  = 'your secret';
 
-const API_QUERY_URL = 'https://data.gateio.io/';
-const API_TRADE_URL = 'https://api.gateio.io/';
+const API_QUERY_URL = 'https://data.gateio.co/';
+const API_TRADE_URL = 'https://api.gateio.co/';
 const PAIRS_URL = 'api2/1/pairs';
 const MARKETINFO_URL = 'api2/1/marketinfo';
 const MARKETLIST_URL = 'api2/1/marketlist';
